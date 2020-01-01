cf8ee49def3e8570cc9231681cb0c3f384b1d618
Nigurasi <Nigurasi@localhost.localdomain>
2020-01-01 23:13:40 +0100

    add random initialization of a matrix

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 3
+
+void printMatrix(int a[N][N])
+{
+    for(int i = 0; i < N; ++i)
+    {
+	for(int j = 0; j < N; ++j)
+	{
+	    printf("%d ", a[i][j]);
+	}
+	printf("\n");
+    }
+}
+
+void initializeMatrix(int a[N][N])
+{
+    for(int i = 0; i < N; ++i)
+    {
+	for(int j = 0; j < N; ++j)
+	{
+	    a[i][j] = rand()%100;
+	}
+    }
+}
+
+void inverseMatrix(int a[N][N])
+{
+}
 
 int main() 
 {
-	printf("Hello Wirdl");
+    srand(time(NULL));
+    int A[N][N];
+    initializeMatrix(A);
+    printMatrix(A);
 
-	return 0;
+    return 0;
 }
