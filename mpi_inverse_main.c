8e60bb55be7dfc029d2361640c4cfd7a13734737
Nigurasi <anna.alberska@wp.pl>
2020-01-22 13:12:17 -0500

    small fix & add matrix initialization

--- mpi_inverse_main.c
+++ mpi_inverse_main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10
+typedef unsigned int uint;
+
+void printMatrixInt(int* a)
+{
+    for(uint i = 0; i < N; ++i)
+    {
+	for(uint j = 0; j < N; ++j)
+	{
+	    printf("%d ", a[i * N + j]);
+	}
+	printf("\n");
+    }
+}
+
+void initializeMatrix(int* a)
+{
+    for(uint i = 0; i < N; ++i)
+    {
+	for(uint j = 0; j < N; ++j)
+	{
+	    a[i * N + j] = rand()%10;
+	}
+    }
+}
 
 int main(int argc, char** argv)
 {
+    srand(time(NULL));
+
+    int A[N * N];
+    float inverseMatrix[N * N];
+
     MPI_Status status;
     int process_id, root_id, processes;
     int error;
@@ -13,9 +46,13 @@ int main(int argc, char** argv)
     error = MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
     error = MPI_Comm_size(MPI_COMM_WORLD, &processes);
 
-    if (process_id == root_process)
+    if (process_id == root_id)
     {
 	printf("I'm root process.\n");
+
+        initializeMatrix(A);
+	printf("Matrix:\n");
+	printMatrixInt(A);
     }
     else
     {
